b8b0cca3a0b7b88e28d325ab86a420c6d3c6ba14
DESKTOP-1HBU2N7\User <rzkn000@gmail.com>
2025-06-30 17:09:25 +0400

    Code improvement by splitting the common codes in separate files

---
+++ assets/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#footer-placeholder").load("footer.html");
+  });
+
+
+// $(window).on('scroll', function() {
+//     $('.back-to-top').toggle($(this).scrollTop() > 200);
+//   });
